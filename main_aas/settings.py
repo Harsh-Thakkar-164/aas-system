bf8ad1a64655cf1dcfb32d54441dc6fc491a670f
Harsh-Thakkar-164 <98619363+Harsh-Thakkar-164@users.noreply.github.com>
2022-01-31 20:21:35 +0530

    Update settings.py

--- main_aas/settings.py
+++ main_aas/settings.py
@@ -114,10 +114,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+ROOT_PATH = os.path.dirname('static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(ROOT_PATH, "static"),
 ]
 
 
