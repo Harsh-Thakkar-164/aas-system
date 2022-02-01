44e66f9f14026060b739f91d549acdfdd981e898
Harsh-Thakkar-164 <98619363+Harsh-Thakkar-164@users.noreply.github.com>
2022-02-01 15:01:37 +0530

    Commit

--- main_aas/settings.py
+++ main_aas/settings.py
@@ -11,11 +11,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'd+-z=5rj97w4i+m8w$=tatwji%_q^6k@be@0a#qdpj^km*jrjq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '192.168.0.100','aas-system.herokuapp.com']
-
-DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
@@ -116,11 +114,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT= os.path.join(BASE_DIR,"staticfiles")
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
