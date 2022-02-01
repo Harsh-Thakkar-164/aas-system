115d0f6e9ab9c8e70185582cbb2593af2bf5bcf0
Harsh-Thakkar-164 <98619363+Harsh-Thakkar-164@users.noreply.github.com>
2022-02-01 23:36:51 +0530

    Update settings.py

--- main_aas/settings.py
+++ main_aas/settings.py
@@ -123,7 +123,7 @@ STATICFILES_DIRS = [
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'accidentsecurities@gmail.com'
