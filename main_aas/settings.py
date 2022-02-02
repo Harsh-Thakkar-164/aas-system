048148ed0329b20ecc93a4eb4051a634889d53cc
Harsh-Thakkar-164 <98619363+Harsh-Thakkar-164@users.noreply.github.com>
2022-02-02 17:30:43 +0530

    Commit

--- main_aas/settings.py
+++ main_aas/settings.py
@@ -124,7 +124,8 @@ STATICFILES_DIRS = [
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL= True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'accidentsecurities@gmail.com'
 EMAIL_HOST_PASSWORD = 'AccidentSecurities@PS'
